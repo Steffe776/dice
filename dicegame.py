371790ed46384b4e952a406b58eaa03436d3f473
unknown <Stefan.marinkovic@botkyrka.skola.se>
2019-11-28 09:10:32 +0100

    new update

--- dicegame.py
+++ dicegame.py
@@ -1,10 +1,19 @@
 import random 
+import time
+dice = input("Tjena, vill du slå en tärning? (ja/nej) ") 
+num = random.randint(1,6)
+while dice == "ja":
+    result = random.randint(1,6)
+    time.sleep(2) 
+    print (result)
+    dice = input ("Vill du slå en tärning? (ja/nej) ")
+else:
+    time.sleep(2)
+    print ("Ok, ha det bra")
 
-dice = input("Vill du slå en tärning?")
-num = random.randrange(1,7)
-if dice == "Ja":
-    print(num)
-    dice = import("Vill du slå en tärning?")
+    
+
+
     
 
 
